1d72de5d089c9ca2ebe9912df5cbff0d3233d133
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2017-11-22 03:21:31 +0530

    added serverES6

--- src/serverES6/routes.js
+++ src/serverES6/routes.js
@@ -3,8 +3,13 @@ import express from "express";
 const router = express.Router();
 
 router.get(
-	"/",
+	"/*",
 	(req, res) => {
+
+		const data = {
+			root : `${__dirname}/../../dist/`,
+		};
+
 		res.json(
 			{
 				response : "response from home",
